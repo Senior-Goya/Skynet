ab2b5fbe7547d7c7469233cd6e3dcc788d6d39ca
Jorge Rivas <59454582+Senior-Goya@users.noreply.github.com>
2021-01-04 20:06:16 -0500

    adding Angular

---
+++ skynewClient/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { IPagination } from './Models/pagination';
+import { IProduct } from './Models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'skynewClient';
+  products: IProduct[];
+
+  constructor(private http: HttpClient )
+  {
+
+  }
+
+  ngOnInit()
+  {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination) =>
+    {
+      this.products = response.data;
+      
+    }, error => {
+      console.log(error);
+    });
+
+  }
+}
